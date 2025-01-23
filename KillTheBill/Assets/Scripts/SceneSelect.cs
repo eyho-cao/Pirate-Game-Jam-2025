73b298812df25c3190d88c8554e99a3c05d26199
StevenMedeot <55761357+StevenMedeot@users.noreply.github.com>
2025-01-22 21:39:05 -0500

    Level Selection

--- KillTheBill/Assets/Scripts/SceneSelect.cs
+++ KillTheBill/Assets/Scripts/SceneSelect.cs
@@ -7,12 +7,16 @@ public class SceneSelect : MonoBehaviour
     [SerializeField] private GameObject _levelSelectMenu;
     [SerializeField] private int _verticalGap = 250;
     [SerializeField] private int _horizontalGap = 250;
+    [SerializeField] private Vector2 _startPos = new Vector2(0, 0);
     [SerializeField] private int _numPerRow = 6;
     [SerializeField] private GameObject _selectorObject;
 
     void Start(){
         for(int i = 0; i < _numLevels; i++){
-
+            GameObject clone = (GameObject) GameObject.Instantiate(_selectorObject);
+            clone.transform.SetParent(_levelSelectMenu.transform);
+            int rowNum =  (i - (i % _numPerRow)) / _numPerRow;
+            clone.transform.position = new Vector3(_startPos.x + (i % _numPerRow *_horizontalGap), _startPos.y - (rowNum * _verticalGap), 0);
         }
     }
 }
