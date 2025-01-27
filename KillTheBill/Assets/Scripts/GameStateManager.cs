29cf4bd5cd91a8de3d1b61cfa8d479cad6d479e6
eyho-cao <49457118+eyho-cao@users.noreply.github.com>
2025-01-27 14:52:17 -0500

    updated references from _playerstate to directly reference PlayerStateEngine.Instance
    As Per @StevenMedeot's suggestion

--- KillTheBill/Assets/Scripts/GameStateManager.cs
+++ KillTheBill/Assets/Scripts/GameStateManager.cs
@@ -18,13 +18,11 @@ public class GameStateManager : MonoBehaviour
     [SerializeField] private PauseSystem _pauseSystem;
     
 
-    private PlayerStateEngine _playerState;
     private GameState _currentGameState = GameState.GS_RUNNING;
 
 
     void Start()
     {
-        _playerState = PlayerStateEngine.Instance;
         // Get all enemy object sin scene
     }
 
@@ -68,7 +66,7 @@ public class GameStateManager : MonoBehaviour
     }
 
     private void updateUI() {
-        int numWeaponsLeft = _playerState.GetNumWeaponsInQueue();
+        int numWeaponsLeft = PlayerStateEngine.Instance.GetNumWeaponsInQueue();
         _countText.text = "Weapons: " + numWeaponsLeft;
 
         int stars = getNumStarsEarned();
@@ -93,8 +91,8 @@ public class GameStateManager : MonoBehaviour
   
     private void checkPlayerWeaponCount() {
         
-        if(_playerState){
-            int numWeaponsLeft = _playerState.GetNumWeaponsInQueue();
+        if(PlayerStateEngine.Instance){
+            int numWeaponsLeft = PlayerStateEngine.Instance.GetNumWeaponsInQueue();
             _countText.text = "Weapons: " + numWeaponsLeft;
             if(numWeaponsLeft <= 0) {
                 StartCoroutine(WaitForGameCompleteState());
@@ -103,7 +101,7 @@ public class GameStateManager : MonoBehaviour
     }
 
     private int getNumStarsEarned() {
-        int numWeaponsLeft = _playerState.GetNumWeaponsInQueue();
+        int numWeaponsLeft = PlayerStateEngine.Instance.GetNumWeaponsInQueue();
         int i = 0;
         for (i = 0; i < _starShotRequirements.Length; i++) {
             if(numWeaponsLeft < _starShotRequirements[i]){
